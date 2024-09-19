43d9780a74d89cc34957638812a1b719162b2723
Nagy Zétény <nagyzeteny6@gmail.com>
2024-09-19 17:01:43 +0200

    store scores in an array

--- 01-nullable/teszt-kiertekeles/TesztKiertekeles_Lib/Result.cs
+++ 01-nullable/teszt-kiertekeles/TesztKiertekeles_Lib/Result.cs
@@ -6,26 +6,19 @@
         private const double PASSING_PERCENTAGE = .4;
 
         public string? Name { get; init; }
-        public int? Score1 { get; init; }
-        public int? Score2 { get; init; }
-        public int? Score3 { get; init; }
-        public int? Score4 { get; init; }
-        public int? Score5 { get; init; }
+        public int?[] Score { get; init; }
 
         public Result(string input)
         {
             string[] parts = input.Split(';');
 
             Name = string.IsNullOrWhiteSpace(parts[0]) ? null : parts[0];
-
-            Score1 = ParseNullableInt(parts[1]);
-            Score2 = ParseNullableInt(parts[2]);
-            Score3 = ParseNullableInt(parts[3]);
-            Score4 = ParseNullableInt(parts[4]);
-            Score5 = ParseNullableInt(parts[5]);
+            Score = parts[1..]
+                .Select(x => int.TryParse(x, out int value) ? value : (int?)null)
+                .ToArray();
         }
 
-        public int Total => (Score1 ?? 0) + (Score2 ?? 0) + (Score3 ?? 0) + (Score4 ?? 0) + (Score5 ?? 0);
+        public int Total => Score.Sum() ?? 0;
 
         public double Percentage => (double)Total / (double)MAX_SCORE;
 
@@ -33,24 +26,14 @@
 
         public override string ToString()
         {
-            return $"1. feladat: {(Score1 is null ? "-" : $"{Score1} pont")}" +
-                $"\n2. feladat: {(Score2 is null ? "-" : $"{Score2} pont")}" +
-                $"\n3. feladat: {(Score3 is null ? "-" : $"{Score3} pont")}" +
-                $"\n4. feladat: {(Score4 is null ? "-" : $"{Score4} pont")}" +
-                $"\n5. feladat: {(Score5 is null ? "-" : $"{Score5} pont")}" +
+            return $"1. feladat: {(Score[0] is null ? "-" : $"{Score[0]} pont")}" +
+                $"\n2. feladat: {(Score[1] is null ? "-" : $"{Score[1]} pont")}" +
+                $"\n3. feladat: {(Score[2] is null ? "-" : $"{Score[2]} pont")}" +
+                $"\n4. feladat: {(Score[3] is null ? "-" : $"{Score[3]} pont")}" +
+                $"\n5. feladat: {(Score[4] is null ? "-" : $"{Score[4]} pont")}" +
                 $"\nÖsszesen: {Total} pont";
         }
 
-        public bool SubmittedEmpty => Name is not null
-            && Score1 is null
-            && Score2 is null
-            && Score3 is null
-            && Score4 is null
-            && Score5 is null;
-
-        private static int? ParseNullableInt(string? input)
-        {
-            return int.TryParse(input, out int value) ? value : null;
-        }
+        public bool SubmittedEmpty => Name is not null && Array.TrueForAll(Score, x => x is null);
     }
 }
