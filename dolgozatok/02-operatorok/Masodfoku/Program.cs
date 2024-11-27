a3599e80fa120395ff029a4544f58c4cc85e9f19
Nagy Zétény <nagyzeteny6@gmail.com>
2024-11-27 14:14:15 +0100

    add operatorok dolgozat

---
+++ dolgozatok/02-operatorok/Masodfoku/Program.cs
@@ -0,0 +1,37 @@
+﻿using Masodfoku_Lib;
+
+double a, b, c;
+
+Console.WriteLine("Adja meg az egyik kifejezés együtthatóit:");
+
+Console.Write("a = ");
+a = double.Parse(Console.ReadLine() ?? "");
+
+Console.Write("b = ");
+b = double.Parse(Console.ReadLine() ?? "");
+
+Console.Write("c = ");
+c = double.Parse(Console.ReadLine() ?? "");
+
+var first = new MasodfokuKifejezes(a, b, c);
+
+Console.WriteLine("\n\nAdja meg az másik kifejezés együtthatóit:");
+
+Console.Write("a = ");
+a = double.Parse(Console.ReadLine() ?? "");
+
+Console.Write("b = ");
+b = double.Parse(Console.ReadLine() ?? "");
+
+Console.Write("c = ");
+c = double.Parse(Console.ReadLine() ?? "");
+
+var second = new MasodfokuKifejezes(a, b, c);
+
+Console.WriteLine($"\nAz első kifejezés ({first}) diszkriminánsa: {first.Discriminant}");
+Console.WriteLine($"A második kifejezés ({second}) diszkriminánsa: {second.Discriminant}");
+
+Console.WriteLine($"\nA két kifejezés összege: {first + second}");
+Console.WriteLine($"A két kifejezés különbsége: {first - second}");
+
+Console.WriteLine($"A két kifejezés {(first != second ? "nem " : "")}egyenlő.");
---
+++ dolgozatok/02-operatorok/Masodfoku_Lib/MasodfokuKifejezes.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Masodfoku_Lib
+{
+    public class MasodfokuKifejezes(double a, double b, double c)
+    {
+        public double A { get; init; } = a;
+        public double B { get; init; } = b;
+        public double C { get; init; } = c;
+
+        public double Discriminant => Math.Pow(B, 2) - (4 * A * C);
+
+        public override string ToString() => $"{A}x^2+{B}x+{C}";
+
+        public static MasodfokuKifejezes operator +(MasodfokuKifejezes a, MasodfokuKifejezes b)
+        {
+            return new MasodfokuKifejezes(a.A + b.A, a.B + b.B, a.C + b.B);
+        }
+
+        public static MasodfokuKifejezes operator -(MasodfokuKifejezes a, MasodfokuKifejezes b)
+        {
+            return new MasodfokuKifejezes(a.A - b.A, a.B - b.B, a.C - b.B);
+        }
+
+        public static bool operator ==(MasodfokuKifejezes a, MasodfokuKifejezes b) => a.Equals(b);
+        public static bool operator !=(MasodfokuKifejezes a, MasodfokuKifejezes b) => !a.Equals(b);
+
+        public override int GetHashCode() => HashCode.Combine(A.GetHashCode(), B.GetHashCode(), C.GetHashCode());
+        
+        public override bool Equals(object? obj)
+        {
+            if (obj is not MasodfokuKifejezes) return false;
+
+            return Math.Max(A, (obj as MasodfokuKifejezes).A) % Math.Min(A, (obj as MasodfokuKifejezes).A) == 0
+                && Math.Max(B, (obj as MasodfokuKifejezes).B) % Math.Min(B, (obj as MasodfokuKifejezes).B) == 0
+                && Math.Max(C, (obj as MasodfokuKifejezes).C) % Math.Min(C, (obj as MasodfokuKifejezes).C) == 0;
+        }
+    }
+}
